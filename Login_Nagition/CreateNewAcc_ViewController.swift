e486430fb55e7810284d6f962123d6da20ac52cd
Doãn Tuấn <tuanngodoan@hotmail.com>
2016-11-21 16:46:33 +0700

    Fail check

---
+++ Login_Nagition/CreateNewAcc_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateNewAcc_ViewController.swift
+//  Login_Nagition
+//
+//  Created by Doãn Tuấn on 11/21/16.
+//  Copyright © 2016 doantuan. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewAcc_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Login_Nagition/ViewController.swift
+++ Login_Nagition/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var acc_Text: UITextField!
+    @IBOutlet weak var pass_Text: UITextField!
+    var user: [String:String] = ["admin":"admin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func checkLogin() -> Bool{
+        var check:Bool = false
+        if let pass = acc_Text.text {
+            if pass == pass_Text.text{
+                check = true
+            }
+        }
+         return check
+    }
+    
+    func alertNotif(title: String,mess: String, idef: String){
+        let alert = UIAlertController(title: title, message: mess, preferredStyle: UIAlertControllerStyle.alert)
+       
+        alert.addAction(UIAlertAction(title: "OKE", style: UIAlertActionStyle.default, handler:nil))
+        
+        self.present(alert, animated: true, completion:
+            ({
+        
+            self.pushView(idef: idef)
+            
+        }))
+        
+    }
+    func alertNotif(title: String,mess: String){
+        let alert = UIAlertController(title: title, message: mess, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "OKE", style: UIAlertActionStyle.default, handler:nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    func pushView(idef: String){
+        let view = self.storyboard?.instantiateViewController(withIdentifier: idef)
+        
+        self.navigationController?.pushViewController(view!, animated: true)
+    }
+    
+    @IBAction func Login_Button(_ sender: Any) {
+        if checkLogin() == true {
+            alertNotif(title:"Login Thành Công", mess: "Bạn đã đăng nhập thành công vào tài khoản ",idef: "ViewInfo")
+        }else{
+            alertNotif(title:"Login NO Thành Công", mess: "Bạn đã đăng nhập NO thành công vào tài khoản ")
+        }
+        
+        }
 
+    @IBAction func Create_button(_ sender: Any) {
+        pushView(idef: "ViewCreate")
+    }
+   
+    
+    @IBAction func forgot_button(_ sender: Any) {
+        pushView(idef: "ViewForgot")
+    }
+    
 }
 
